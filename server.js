8982a42ff25b85beeba0bd7df3fbeec374ff1e73
Kennedy Mugera Karimi <kennedymugera@gmail.com>
2021-03-31 13:25:20 +0300

    Finished GraphQL Server

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+//GraphQL Works at the application layer unlike, say, MySQL which works at the data layer thus allowing you to use any type of database.
+//Unlike API's which are all or nothing, GraphQL allows you to only request for the specific items of data you need this trimming down size of the request.
+//Large SW i.e. Facebook with millions of users can save on bandwidth, process requests faster by only requesting for the items of data needed and nothing more, as would happen with an API.
+
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const cors = require('cors');
